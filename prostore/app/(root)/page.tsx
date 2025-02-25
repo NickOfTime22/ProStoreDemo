f5547a9a54a4a89c2b4ae289469610ed91ce0673
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-02-25 16:15:37 -0500

    Added Item cards
    Finished step 17 and section 2.

--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -1,10 +1,21 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 // Sets a custom page title
 export const metadata = {
   title: "Home",
 };
 
 const Homepage = () => {
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      ></ProductList>
+    </>
+  );
 };
 
 export default Homepage;
