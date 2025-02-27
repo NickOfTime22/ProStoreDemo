e40d45fb31543eba14348690ed606a061bb589b4
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-02-27 16:00:57 -0500

    User registering complete
    Through step 41, error handling does not exist yet

--- prostore/lib/actions/user.actions.ts
+++ prostore/lib/actions/user.actions.ts
@@ -1,7 +1,9 @@
 "use server";
-import { signInFormSchema } from "../validators";
+import { signInFormSchema, signUpFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
+import { hashSync } from "bcrypt-ts-edge";
+import { prisma } from "@/db/prisma";
 
 // Sign in the user with credentials
 export async function signInWithCredentials(
@@ -29,4 +31,42 @@ export async function signInWithCredentials(
 // Sign out user
 export async function signOutUser() {
   await signOut();
+}
+
+// Register user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      confirmPassword: formData.get("confirmPassword"),
+    });
+
+    // used to log user in after registering
+    const plainPassword = user.password;
+
+    user.password = hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn("credentials", {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: "User registered successfully" };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    return { success: false, message: "User could not be registered" };
+  }
 }
