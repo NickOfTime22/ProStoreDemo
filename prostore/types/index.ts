594c6f83949ea77ae9582959296612a7daabe9ac
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-02-28 10:00:58 -0500

    Added Cart model
    completed step 45 of course

--- prostore/types/index.ts
+++ prostore/types/index.ts
@@ -1,8 +1,15 @@
-import { insertProductsSchema } from "@/lib/validators";
-import {z} from "zod";
+import { z } from "zod";
+import {
+  cartItemSchema,
+  insertCartSchema,
+  insertProductsSchema,
+} from "@/lib/validators";
 
 export type Product = z.infer<typeof insertProductsSchema> & {
   id: string;
   rating: string;
   createdAt: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
