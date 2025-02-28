51cdc0063a13cc57eed0d92b40ad6f223ee55485
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-02-28 13:03:55 -0500

    Auth changes made app break
    Not sure why app isnt working. Branched off of main to see exactly where the error occurs

---
+++ prostore/components/shared/product/add-to-cart.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { CartItem } from "@/types";
+import { useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+import { addItemToCart } from "@/lib/actions/cart.actions";
+
+const AddToCart = ({ item }: { item: CartItem }) => {
+  const router = useRouter();
+
+  const handleAddToCart = async () => {
+    const res = await addItemToCart(item);
+
+    if (!res.success) {
+      toast.error(res.message);
+      return;
+    }
+
+    // Handles success add to cart
+    toast.success("", {
+      description: `${item.name} added to cart`,
+      action: {
+        label: "Go to Cart",
+        onClick: () => router.push("/cart"),
+      },
+    });
+  };
+
+  return (
+    <Button className="w-full" type="button" onClick={handleAddToCart}>
+      <Plus /> Add To Cart
+    </Button>
+  );
+};
+
+export default AddToCart;
