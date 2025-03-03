99e64c15275f14d2a57d75f64d619d3ca0f44424
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-03-03 13:35:41 -0500

    Updated middleware.ts
    Prod might work now. Prisma modules were apparently causing issues

--- prostore/middleware.ts
+++ prostore/middleware.ts
@@ -1,1 +1,8 @@
 export { auth as middleware } from "@/auth";
+
+export const config = {
+  unstable_allowDynamic: [
+    "**/node_modules/@auth/prisma-adapter/**",
+    "**/node_modules/**prisma**/**",
+  ],
+};
