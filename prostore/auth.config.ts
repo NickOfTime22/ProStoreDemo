be412e5fd3800ce43201dc7cb6a32893e9aba2a9
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-03-05 14:13:28 -0500

    Update auth.config.ts
    Added route protections

--- prostore/auth.config.ts
+++ prostore/auth.config.ts
@@ -7,6 +7,23 @@ export const authConfig = {
   providers: [], // Required by NextAuthConfig
   callbacks: {
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected route
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie
