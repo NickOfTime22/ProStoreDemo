2a9aaf4c7de53e146f81a574cdefe4cc450fdd99
Nick Battista <52249671+NickOfTime22@users.noreply.github.com>
2025-03-11 08:17:00 -0400

    Admin overview and admin guards

--- prostore/app/admin/overview/page.tsx
+++ prostore/app/admin/overview/page.tsx
@@ -13,12 +13,16 @@ import { formatCurrency, formatDateTime, formatNumber } from "@/lib/utils";
 import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import Charts from "./charts";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
 };
 
 const AdminOverviewPage = async () => {
+  await requireAdmin();
+
   const session = await auth();
 
   if (session?.user?.role !== "admin") {
@@ -83,7 +87,13 @@ const AdminOverviewPage = async () => {
           <CardHeader>
             <CardTitle>Overview</CardTitle>
           </CardHeader>
-          <CardContent>{/* CHART HERE */}</CardContent>
+          <CardContent>
+            <Charts
+              data={{
+                salesData: summary.salesData,
+              }}
+            />
+          </CardContent>
         </Card>
         <Card className="col-span-3">
           <CardHeader>
